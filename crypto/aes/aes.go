e272b2b2dad5542d5d763dfb0dd4ef3fc7c1be04
陈飞 <fei.chen@melot.cn>
2020-11-16 23:12:42 +0800

    aes Encrypt & Decrypt return with []byte

--- crypto/aes/aes.go
+++ crypto/aes/aes.go
@@ -7,40 +7,52 @@ import (
 )
 
 func Encrypt(text []byte, key []byte) (string, error) {
+	b, err := EncryptHex(text, key)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(b), nil
+}
+func EncryptHex(text []byte, key []byte) ([]byte, error) {
 	var iv = key[:aes.BlockSize]
 	encrypted := make([]byte, len(text))
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	encrypter := cipher.NewCFBEncrypter(block, iv)
 	encrypter.XORKeyStream(encrypted, text)
-	return hex.EncodeToString(encrypted), nil
+
+	return encrypted, nil
 }
 func EncryptSting(text string, key []byte) (string, error) {
 	return Encrypt([]byte(text), key)
 }
 
 func Decrypt(encrypted string, key []byte) ([]byte, error) {
+	src, err := hex.DecodeString(encrypted)
+	if err != nil {
+		return nil, err
+	}
+	return DecryptHex(src, key)
+}
+
+func DecryptHex(encryptedBytes []byte, key []byte) ([]byte, error) {
 	var err error
 	defer func() {
 		if e := recover(); e != nil {
 			err = e.(error)
 		}
 	}()
-	src, err := hex.DecodeString(encrypted)
-	if err != nil {
-		return nil, err
-	}
 	var iv = key[:aes.BlockSize]
-	decrypted := make([]byte, len(src))
+	decrypted := make([]byte, len(encryptedBytes))
 	var block cipher.Block
-	block, err = aes.NewCipher([]byte(key))
+	block, err = aes.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
 	decrypter := cipher.NewCFBDecrypter(block, iv)
-	decrypter.XORKeyStream(decrypted, src)
+	decrypter.XORKeyStream(decrypted, encryptedBytes)
 	return decrypted, nil
 }
 
