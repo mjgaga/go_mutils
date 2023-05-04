61341acc8df9ad32e58d16d6424c274e54693b9c
chenfei <fei.chen@melot.cn>
2023-05-04 15:47:15 +0800

    post action missing ctx

--- request/http.go
+++ request/http.go
@@ -32,6 +32,7 @@ func (client *HttpClient) Head(ctx context.Context, url string, headers ...*Head
 func (client *HttpClient) Get(ctx context.Context, url string, headers ...*Header) (resBody []byte, statusCode int, header http.Header, err error) {
 	req, _ := http.NewRequest(http.MethodGet, url, nil)
 	req = req.WithContext(ctx)
+
 	for _, head := range headers {
 		if head == nil {
 			continue
@@ -53,6 +54,7 @@ func (client *HttpClient) Get(ctx context.Context, url string, headers ...*Heade
 func (client *HttpClient) Post(ctx context.Context, url string, body []byte, headers ...*Header) (resBody []byte, statusCode int, header http.Header, err error) {
 	bodyReader := bytes.NewReader(body)
 	req, _ := http.NewRequest(http.MethodPost, url, bodyReader)
+	req = req.WithContext(ctx)
 
 	for _, head := range headers {
 		if head == nil {
